ca3563825cc26bdff5e578b62794bf3ffec999bc
Kushal Agarwal <67742794+kushal-agarwal@users.noreply.github.com>
2024-10-18 09:39:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       {/* <img src="/TechAid_Logo.png"/> */}
-      <h1 className="text-3xl font-bold text-center mb-4">TechAid</h1>
+      <h1 className="text-3xl font-bold text-center mb-4 heading">TechAid</h1>
 
       <div className="chat-container rounded-lg shadow-md p-4">
         <ChatHistory chatHistory={chatHistory} />
@@ -70,7 +70,7 @@ const App = () => {
           onChange={handleUserInput}
         />
         <button
-          className="px-4 py-2 ml-2 rounded-lg bg-blue-500 text-white hover:bg-black-600 focus:outline-none"
+          className="px-4 py-2 ml-2 rounded-lg bg-green-500 text-white hover:bg-black-600 focus:outline-none"
           onClick={sendMessage}
           disabled={isLoading}
         >
@@ -78,7 +78,7 @@ const App = () => {
         </button>
       </div>
       <button
-        className="mt-4 block px-4 py-2 rounded-lg bg-gray-400 text-white hover:bg-white-500 focus:outline-none"
+        className="mt-4 block px-4 py-2 rounded-lg bg-red-400 text-white hover:bg-white-500 focus:outline-none"
         onClick={clearChat}
       >
         Clear Chat
